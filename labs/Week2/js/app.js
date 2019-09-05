11b744166b2dbd63afbd34c7315bc25015811207
Travis Faas <travis.faas@gmail.com>
2019-09-05 16:58:34 -0400

    Finishing Rain Scaffold

--- labs/Week2/js/app.js
+++ labs/Week2/js/app.js
@@ -1,6 +1,6 @@
 class Drop {
     constructor() {
-        this.x = 40;
+        this.x = Math.random() * 400;
         this.y = 0;
     }
 
@@ -11,7 +11,40 @@ class Drop {
     }
 }
 
-var d = new Drop();
+class RainManager {
+    constructor() {
+        this.drops = [];
+    }
+
+    createDrop() {
+        //make a new drop
+        var newDrop = new Drop();
+        
+        //add this drop to our collection of drops
+        this.drops.push(newDrop);
+    }
+
+    update() {
+        for(var i = 0; i < this.drops.length; i++) {
+            this.drops[i].update();
+        }
+    }
+}
+
+class Ground {
+    //constructor
+        //set the starting color
+        //start the drop hit count
+
+    //update - draws the rectangle to the screen
+
+    //drop hit - called when a rain drop gets low enough (how do you inform it?)
+        //change the color for every ten rain drops hit
+
+}
+
+//global variables
+var rainManager = new RainManager();
 
 //Run once before the application starts
 function setup() {
@@ -20,17 +53,15 @@ function setup() {
 
 //runs 60 times a second, or so
 function draw() {
-    d.update();
-}
 
-class RainManager {
-    constructor() {
-        this.drops = [];
+    //clear out background
+    background(255);
+
+    //create a new drop on a 1% chance
+    if(Math.random() < .05) {
+        rainManager.createDrop();
     }
 
-    createDrop() {
-        //stub
-        //TODO: complete
-    }
+    rainManager.update();
 }
 
