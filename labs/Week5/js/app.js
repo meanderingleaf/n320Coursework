298fdc70a698fb7bd83d8573410287645edbabdb
Travis <travis.faas@gmail.com>
2019-09-26 14:27:46 -0400

    Adding Cycle

--- labs/Week5/js/app.js
+++ labs/Week5/js/app.js
@@ -6,6 +6,22 @@ Vue.component("student-card", {
 var app = new Vue({
     el: "#app",
     data: {
-        currentStudent: { name: "Sienna", skill: 2, joy: 0 }
+        students: [
+            { name: "Sienna", skill: 2, joy: 0 },
+            { name: "Cyan", skill: 0, joy: 5 },
+            { name: "Magenta", skill: 3, joy: 3 }
+        ],
+        currentStudent: { name: "Sienna", skill: 2, joy: 0 },
+        curStudentId: 0
+    },
+    methods: {
+        arrowClicked: function() {
+            this.curStudentId ++;
+            this.currentStudent = this.students[this.curStudentId];
+
+            if(this.curStudentId >= this.students.length-1) {
+                this.curStudentId = -1;
+            }
+        }
     }
 })
