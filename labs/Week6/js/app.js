fd87b5c3c8a7daa57d99d9fe4098a5d67393d41c
Travis <travis.faas@gmail.com>
2019-10-03 14:16:54 -0400

    Clickable grid

--- labs/Week6/js/app.js
+++ labs/Week6/js/app.js
@@ -10,5 +10,18 @@ var app = new Vue({
             [ 0,0,0,0,0,0,0 ],
             [ 0,0,0,0,0,0,0 ]
         ]
+    },
+    methods: {
+        selectCell: function(row, col) {
+
+            //copy grid to a temporary var
+            var tempGrid = this.grid.slice(0);
+
+            //modify the cloned version
+            tempGrid[row][col] = "A";
+
+            //replace
+            this.grid = tempGrid;
+        }
     }
 })
